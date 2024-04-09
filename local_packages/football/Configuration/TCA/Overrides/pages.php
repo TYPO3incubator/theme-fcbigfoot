b79f3ed57988de32bf2b2c49fa4ac10a2824f66d
Lisa Kreitz <l.kreitz@queo-group.com>
2024-04-09 15:03:10 +0100

    [TASK] Add fields for footer configuration to (root) site configuration

--- local_packages/football/Configuration/TCA/Overrides/pages.php
+++ local_packages/football/Configuration/TCA/Overrides/pages.php
@@ -6,6 +6,75 @@ call_user_func(function()
      * Temporary variables
      */
     $extensionKey = 'football';
+    $table = 'pages';
+
+    $fields = [
+        'data_privacy' => [
+            'label' => 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:pages.data_privacy',
+            'displayCond' => 'FIELD:is_siteroot:REQ:true',
+            'config' => [
+                'type' => 'link',
+                'allowedTypes' => ['page','url'],
+            ],
+        ],
+        'copyright' => [
+            'label' => 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:pages.copyright',
+            'displayCond' => 'FIELD:is_siteroot:REQ:true',
+            'config' => [
+                'type' => 'input',
+                'size' => 100
+            ],
+        ],
+        'social' => [
+            'label' => 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:pages.social',
+            'displayCond' => 'FIELD:is_siteroot:REQ:true',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_football_domain_model_link',
+                'foreign_table_field' => 'parenttable',
+                'foreign_field' => 'parentid',
+                'appearance' => [
+                    'useSortable' => true
+                ],
+                'overrideChildTca' => [
+                    'types' => [
+                        [
+                            'showitem' => 'url,icon'
+                        ],
+                    ],
+                    'columns' => [
+                        'url' => [
+                            'config' => [
+                                'allowedTypes' => ['page', 'url'],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    /**
+     * add fields to pages
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $fields);
+
+    /**
+     * add new palette for footer config
+     */
+    $GLOBALS['TCA']['pages']['palettes']['footer'] = [
+        'label' => 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:pages.palettes.footer',
+        'showitem' => 'data_privacy,--linebreak--,copyright,--linebreak--,social'
+    ];
+
+    /**
+     * Add palette footer in new tab in pages
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        $table,
+        '--div--;LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:pages.tab.footer,
+            --palette--;;footer'
+    );
 
     /**
      * Default PageTS for Football
