c0705b423a3644df6e7694f6561ca30a7bf8fb33
Lisa Kreitz <l.kreitz@queo-group.com>
2024-04-10 16:48:50 +0100

    [TASK] Add event teaser placeholder contentelement

---
+++ local_packages/football/Configuration/TCA/Overrides/tt_content.eventteaser.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+call_user_func(static function() {
+    $ext = 'football';
+    $cType = 'event_teaser';
+
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+        'tt_content',
+        'CType',
+        [
+            'label' => 'LLL:EXT:' . $ext . '/Resources/Private/Language/locallang_db.tt_content.' . $cType . '.xlf:title',
+            'value' => $cType,
+            'group' => 'special',
+            'icon' => 'content-text-teaser',
+            'description' => 'LLL:EXT:' . $ext . '/Resources/Private/Language/locallang_db.tt_content.' . $cType . '.xlf:description',
+        ]
+    );
+
+    $GLOBALS['TCA']['tt_content']['types'][$cType] = [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            header,header_layout,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:appearance,
+            --palette--;;frames,
+            --palette--;;appearanceLinks,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+    ',
+    ];
+});
