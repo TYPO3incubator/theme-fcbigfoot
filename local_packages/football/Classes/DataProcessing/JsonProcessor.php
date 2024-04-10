164afa1978d60843f062f863ab77d4b397efd43f
Lisa Kreitz <l.kreitz@queo-group.com>
2024-04-10 18:09:02 +0100

    [TASK] Add JsonProcessor to get data from jsonFiles as array

---
+++ local_packages/football/Classes/DataProcessing/JsonProcessor.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SurfcampTeam4\Football\DataProcessing;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+use TYPO3\CMS\Frontend\Typolink\LinkFactory;
+
+/**
+ * Class JsonProcessor
+ * @package SurfcampTeam4\Football\DataProcessing
+ */
+class JsonProcessor implements DataProcessorInterface
+{
+    /**
+     * @param ContentObjectRenderer $cObj
+     * @param array $contentObjectConfiguration
+     * @param array $processorConfiguration
+     * @param array $processedData
+     * @return array
+     */
+    public function process(
+        ContentObjectRenderer $cObj,
+        array $contentObjectConfiguration,
+        array $processorConfiguration,
+        array $processedData
+    ): array {
+        if (isset($processorConfiguration['if.']) && !$cObj->checkIf($processorConfiguration['if.'])) {
+            return $processedData;
+        }
+        $jsonFile = $cObj->stdWrapValue('jsonFile', $processorConfiguration ?? []);
+
+        $linkFactory = GeneralUtility::makeInstance(LinkFactory::class);
+        // ToDo: Get absolute Uri for file link
+        $jsonFileUrl = $linkFactory->createUri($jsonFile);
+
+        $targetVariableName = $cObj->stdWrapValue('as', $processorConfiguration, 'jsonData');
+        $processedData[$targetVariableName] = json_decode(file_get_contents($jsonFileUrl->getUrl()), true);
+
+        return $processedData;
+    }
+}
